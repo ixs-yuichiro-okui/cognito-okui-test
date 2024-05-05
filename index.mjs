c3aa5411fdd8ff4d257d7bef4c08332dcf324cb6
yuichiro-okui-local <yuichiro.okui@ixs.co.jp>
2024-05-05 22:29:59 +0900

    commit temporary

--- index.mjs
+++ index.mjs
@@ -9,10 +9,7 @@ import { CognitoIdentityClient } from "@aws-sdk/client-cognito-identity";
 export const handler = async (event) => {
   console.log(`token; ${event.token}`);
   
-  const bucketParams = {
-    Bucket: "my-okui-free-bucket",
-  };
-  const command = new ListObjectsCommand(bucketParams);
+
 
   // Access user attributes passed from the Cognito Authorizer
   const claims = event.requestContext.authorizer.claims;
@@ -25,12 +22,18 @@ export const handler = async (event) => {
 
   // 'COGNITO_ID' has the format 'cognito-idp.REGION.amazonaws.com/COGNITO_USER_POOL_ID'
   const userPoolId = "cognito-idp.ap-northeast-1.amazonaws.com/ap-northeast-1_cCNpAdnKi";
-  const identityPoolId = "ap-northeast-1:4a45c6c0-1502-42e4-a9a6-3685b560eddf";
+  const identityPoolId = "ap-northeast-1:f09f8d72-e7c9-4bd6-9bce-0fd53e4ec203";
   const region = 'ap-northeast-1';
   const loginData = {
     [userPoolId]: event.headers.Authorization?.split(' ')[1],
   };
   
+  const bucketParams = {
+    Bucket: "mytest-okui-bucket",
+    Prefix: `${username}/`
+  };
+  const command = new ListObjectsCommand(bucketParams);
+
   const cognitoIdentity = {
     credentials: fromCognitoIdentityPool({
       client: new CognitoIdentityClient({region}),
